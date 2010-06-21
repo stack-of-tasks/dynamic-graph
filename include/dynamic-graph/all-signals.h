09dda4346158ff3d5d2cc8b4c1a502138e9f0ca5
Francois Bleibel <fbleibel@gmail.com>
2010-06-21 16:02:30 +0900

    Added forgotten file all-signals.h

---
+++ include/dynamic-graph/all-signals.h
@@ -0,0 +1,19 @@
+/*
+ * all-signals.h
+ *
+ *  Created on: Jun 16, 2010
+ *      Author: blue
+ */
+
+#ifndef ALLSIGNALS_H_
+#define ALLSIGNALS_H_
+
+/// Utility header files including all signals and using namespace dynamicgraph
+
+#include <dynamic-graph/signal.h>
+#include <dynamic-graph/signal-ptr.h>
+#include <dynamic-graph/signal-time-dependant.h>
+
+using namespace dynamicgraph;
+
+#endif /* ALLSIGNALS_H_ */
