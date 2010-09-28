df072e55ef4409d9ab9f0cd3155f1eba35bbf223
Thomas Moulard <thomas.moulard@gmail.com>
2010-09-28 03:47:47 +0200

    Install binaries in bin directory.

--- tools/sot-shell-plugin.sh.cmake
+++ tools/sot-shell-plugin.sh.cmake
@@ -2,4 +2,4 @@
 echo "-- Launching dynamic-graph C shell with default plugins"
 echo "loadPlugin ${CMAKE_INSTALL_PREFIX}/lib/plugin/shell-functions.so" > /tmp/default_script
 echo "loadPlugin ${CMAKE_INSTALL_PREFIX}/lib/plugin/shell-procedure.so" >> /tmp/default_script
-${CMAKE_INSTALL_PREFIX}/bin/${PROJECT_NAME}/test_shell /tmp/default_script
+${CMAKE_INSTALL_PREFIX}/bin/sot-shell /tmp/default_script
