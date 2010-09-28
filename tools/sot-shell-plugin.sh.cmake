470262e25dc6ff91db87725133f800b4b1d0f228
Thomas Moulard <thomas.moulard@gmail.com>
2010-09-28 03:52:00 +0200

    Use mktemp to create temporary file in sot-shell-plugin.sh.

--- tools/sot-shell-plugin.sh.cmake
+++ tools/sot-shell-plugin.sh.cmake
@@ -1,5 +1,6 @@
 #! /bin/sh
+tmp=`mktemp`
 echo "-- Launching dynamic-graph C shell with default plugins"
-echo "loadPlugin ${CMAKE_INSTALL_PREFIX}/lib/plugin/shell-functions.so" > /tmp/default_script
-echo "loadPlugin ${CMAKE_INSTALL_PREFIX}/lib/plugin/shell-procedure.so" >> /tmp/default_script
-${CMAKE_INSTALL_PREFIX}/bin/sot-shell /tmp/default_script
+echo "loadPlugin ${CMAKE_INSTALL_PREFIX}/lib/plugin/shell-functions.so" > $tmp
+echo "loadPlugin ${CMAKE_INSTALL_PREFIX}/lib/plugin/shell-procedure.so" >> $tmp
+${CMAKE_INSTALL_PREFIX}/bin/sot-shell $tmp
