61c02f05a95bd8c07884de86ee356549989eb8d0
Thomas Moulard <thomas.moulard@gmail.com>
2010-09-28 03:42:28 +0200

    Fix shell.sh.cmake.
    Add missing sheebang and generate file in a temporary directory.
    This is a quick hack as there is no reason that the current directory
    would be writeable for the user.

--- tools/shell.sh.cmake
+++ tools/shell.sh.cmake
@@ -1,4 +1,5 @@
+#! /bin/sh
 echo "-- Launching dynamic-graph C shell with default plugins"
-echo "loadPlugin ${CMAKE_INSTALL_PREFIX}/lib/plugin/shell-functions.so" > ./default_script
-echo "loadPlugin ${CMAKE_INSTALL_PREFIX}/lib/plugin/shell-procedure.so" >> ./default_script
-${CMAKE_INSTALL_PREFIX}/bin/${PROJECT_NAME}/test_shell default_script
+echo "loadPlugin ${CMAKE_INSTALL_PREFIX}/lib/plugin/shell-functions.so" > /tmp/default_script
+echo "loadPlugin ${CMAKE_INSTALL_PREFIX}/lib/plugin/shell-procedure.so" >> /tmp/default_script
+${CMAKE_INSTALL_PREFIX}/bin/${PROJECT_NAME}/test_shell /tmp/default_script
