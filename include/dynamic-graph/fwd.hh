20d8b91c039b3d55ba5715f265bbea2dd295beb4
Thomas Moulard <thomas.moulard@gmail.com>
2010-12-24 01:20:20 +0100

    Clean and document factoryStorage.

---
+++ include/dynamic-graph/fwd.hh
@@ -0,0 +1,28 @@
+// Copyright 2010, Thomas Moulard, JRL, CNRS/AIST
+//
+// This file is part of dynamic-graph.
+// dynamic-graph is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// dynamic-graph is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.  You should have
+// received a copy of the GNU Lesser General Public License along with
+// dynamic-graph. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef DYNAMIC_GRAPH_FWD_HH
+# define DYNAMIC_GRAPH_FWD_HH
+
+namespace dynamicgraph
+{
+  class Entity;
+
+  class FactoryStorage;
+  class EntityRegisterer;
+
+} // end of namespace dynamicgraph.
+
+#endif //! DYNAMIC_GRAPH_FWD_HH
