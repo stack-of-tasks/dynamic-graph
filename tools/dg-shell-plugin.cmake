a0cad791befba9f6912ddb85c46d0d395a05430d
Thomas Moulard <thomas.moulard@gmail.com>
2011-01-01 13:13:26 +0100

    Update dg-shell-plugin to match new dg-shell usage.

--- tools/dg-shell-plugin.cmake
+++ tools/dg-shell-plugin.cmake
@@ -3,4 +3,4 @@ tmp=`mktemp`
 echo "-- Launching dynamic-graph C shell with default plugins"
 echo "loadPlugin ${CMAKE_INSTALL_PREFIX}/lib/plugin/shell-functions.so" > $tmp
 echo "loadPlugin ${CMAKE_INSTALL_PREFIX}/lib/plugin/shell-procedure.so" >> $tmp
-${CMAKE_INSTALL_PREFIX}/bin/dg-shell $tmp
+${CMAKE_INSTALL_PREFIX}/bin/dg-shell -i $tmp $@
