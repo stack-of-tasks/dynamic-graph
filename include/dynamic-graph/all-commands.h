2674beca4c41da5d2c21c6553ab91136317007b0
Mansard <nmansard@laas.fr>
2011-01-24 12:06:28 +0100

    IVIGIT.

---
+++ include/dynamic-graph/all-commands.h
@@ -0,0 +1,30 @@
+// -*- mode: c++ -*-
+// Copyright 2010, François Bleibel, Thomas Moulard, Olivier Stasse,
+// JRL, CNRS/AIST.
+//
+// This file is part of dynamic-graph.
+// dynamic-graph is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// dynamic-graph is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.  You should have
+// received a copy of the GNU Lesser General Public License along with
+// dynamic-graph. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef DYNAMIC_GRAPH_ALL_SIGNALS_H
+# define DYNAMIC_GRAPH_ALL_SIGNALS_H
+
+// Utility header files including all signal headers
+
+#include <dynamic-graph/command-bind.h>
+#include <dynamic-graph/command-direct-getter.h>
+#include <dynamic-graph/command-direct-setter.h>
+#include <dynamic-graph/command-getter.h>
+#include <dynamic-graph/command.h>
+#include <dynamic-graph/command-setter.h>
+
+#endif //! DYNAMIC_GRAPH_ALL_SIGNALS_H
