d0acc4077f4984b636cd9ab02ea6e8b1424f8bdf
Francois Keith <francois.keith@inrialpes.fr>
2011-04-08 21:09:34 +0200

    A cleaner way to define the suffix of the dynamic libraries according to each OS

--- tests/interpreter.cpp
+++ tests/interpreter.cpp
@@ -181,21 +181,21 @@ BOOST_AUTO_TEST_CASE (cmd_loadPlugin)
   dynamicgraph::Interpreter shell (&pl);
 
   {
-    RUN_COMMAND ("loadPlugin", "shell-functions.so" " " TESTS_PLUGINDIR);
+    RUN_COMMAND ("loadPlugin", "shell-functions" TESTS_DYNLIBSUFFIX " " TESTS_PLUGINDIR);
     BOOST_CHECK (output.is_empty ());
   }
   {
-    RUN_COMMAND ("loadPlugin", "shell-procedure.so" " " TESTS_PLUGINDIR);
-    BOOST_CHECK (output.is_empty ());
-  }
-
-  {
-    RUN_COMMAND ("unloadPlugin", TESTS_PLUGINDIR "/shell-procedure.so");
+    RUN_COMMAND ("loadPlugin", "shell-procedure" TESTS_DYNLIBSUFFIX " " TESTS_PLUGINDIR);
     BOOST_CHECK (output.is_empty ());
   }
 
   {
-    RUN_COMMAND ("unloadPlugin", TESTS_PLUGINDIR "/shell-functions.so");
+    RUN_COMMAND ("unloadPlugin", TESTS_PLUGINDIR "/shell-procedure" TESTS_DYNLIBSUFFIX);
+    BOOST_CHECK (output.is_empty ());
+  }
+
+  {
+    RUN_COMMAND ("unloadPlugin", TESTS_PLUGINDIR "/shell-functions" TESTS_DYNLIBSUFFIX);
     BOOST_CHECK (output.is_empty ());
   }
   
