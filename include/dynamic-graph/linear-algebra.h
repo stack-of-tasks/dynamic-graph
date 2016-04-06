8cff599b81b705fd79b20f8d939423f90efbf11c
Rohan Budhiraja <budhiraja@laas.fr>
2016-04-06 12:22:18 +0200

    [c++] update the ostream output format for MatrixHomogeneous to [M,N]((,,,),(,,,),(,,,))

--- include/dynamic-graph/linear-algebra.h
+++ include/dynamic-graph/linear-algebra.h
@@ -18,6 +18,7 @@
 #ifndef DYNAMIC_GRAPH_LINEAR_ALGEBRA_H
 #define DYNAMIC_GRAPH_LINEAR_ALGEBRA_H
 #include <Eigen/Core>
+#include <Eigen/Geometry>
 
 namespace dynamicgraph {
   typedef Eigen::MatrixXd Matrix;
