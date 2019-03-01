157f5e62e58adb48ae624a8927b6925aa9e547df
Olivier Stasse <ostasse@laas.fr>
2019-03-01 01:51:53 +0100

    [tests] Improve tests on pool.cpp with respect to exceptions. Add entity test to check writeCompletionList + license modification.

--- tests/entity.cpp
+++ tests/entity.cpp
@@ -1,17 +1,9 @@
-// Copyright 2010 Thomas Moulard.
-//
-// This file is part of dynamic-graph.
-// dynamic-graph is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// dynamic-graph is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// You should have received a copy of the GNU Lesser General Public License
-// along with dynamic-graph.  If not, see <http://www.gnu.org/licenses/>.
+/* Copyright 2010-2019 LAAS, CNRS
+ * Thomas Moulard.
+ *
+ * See LICENSE file
+ *
+ */
 
 #define ENABLE_RT_LOG
 
@@ -140,9 +132,9 @@ BOOST_AUTO_TEST_CASE (writeCompletionList)
     dynamicgraph::PoolStorage::getInstance()->getEntity("my-entity");
 
   output_test_stream output;
-  entity.writeGraph (output);
+  entity.writeCompletionList (output);
 
-  BOOST_CHECK (output.is_equal (""));
+  BOOST_CHECK (output.is_equal ("print\nsignals\nsignalDep\n"));
 }
 
 BOOST_AUTO_TEST_CASE (sendMsg)
