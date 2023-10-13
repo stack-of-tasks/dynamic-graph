134ec15a15cdbf52d7a5146a9ce5f237c979ddb1
Olivier Stasse <olivier.stasse@laas.fr>
2023-10-13 07:32:42 +0200

    unsigned long seems to be too ambiguous for clang switch to uint64_t

--- include/dynamic-graph/command-setter.t.cpp
+++ include/dynamic-graph/command-setter.t.cpp
@@ -90,10 +90,10 @@ Value Setter<E, unsigned>::doExecute() {
 // Template specialization: unsigned long
 //
 template <class E>
-class Setter<E, unsigned long> : public Command {
+class Setter<E, uint64_t> : public Command {
  public:
   /// Pointer to method that sets parameter of type unsigned long
-  typedef void (E::*SetterMethod)(const unsigned long &);
+  typedef void (E::*SetterMethod)(const uint64_t &);
   /// Constructor
   Setter(E &entity, SetterMethod setterMethod, const std::string &docString);
 
@@ -105,17 +105,17 @@ class Setter<E, unsigned long> : public Command {
 };  // Class Setter
 
 template <class E>
-Setter<E, unsigned long>::Setter(E &entity, SetterMethod setterMethod,
+Setter<E, uint64_t>::Setter(E &entity, SetterMethod setterMethod,
                                  const std::string &docString)
     : Command(entity, boost::assign::list_of(Value::UNSIGNEDLONGINT),
               docString),
       setterMethod_(setterMethod) {}
 
 template <class E>
-Value Setter<E, unsigned long>::doExecute() {
+Value Setter<E, uint64_t>::doExecute() {
   const std::vector<Value> &values = getParameterValues();
   // Get parameter
-  unsigned long value = values[0].value();
+  uint64_t value = values[0].value();
   E &entity = static_cast<E &>(owner());
   (entity.*setterMethod_)(value);
   return Value();
